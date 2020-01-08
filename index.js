ab5e54d36a01990d76d628c71224de842c916f9d
Skyler Martyn <skyler.martyn@gmail.com>
2020-01-08 13:34:30 +0100

    Make circle render

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var ballCanvas = document.getElementById("ball-canvas");
+ballCanvas.height = '300';
+ballCanvas.width = '300';
+ballCanvas.style.margin = 'auto';
 var ball = ballCanvas.getContext("2d");
 ball.beginPath();
-ball.arc(100, 75, 50, 0, 2 * Math.PI);
+ball.arc(150, 150, 100, 0, 2 * Math.PI);
 ball.stroke();
