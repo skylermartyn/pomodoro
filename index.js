6d2d266a8396c6507d4431644c17f66206251ed3
Skyler Martyn <skyler.martyn@gmail.com>
2020-01-15 23:02:24 +0100

    Add logic to group cycles into 4 chunks
    The Pomodoro techinque involves grouping sessions into 4 cycles where
    a longer break is triggered at the end. This is now worked into the logic.
    Encouragement messages are incorporated and reset button as well.
    
    The instructions shown upon app load were brief for simplicity's sake,
    so on click they now lead to an expanded description of the Pomodoro
    technique.

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const instructions = document.getElementById('instructions');
 const moreInfo = document.getElementById('more-info');
 const xBox = document.getElementById('x-container');
 const footer = document.querySelector('footer');
+const afterPomo = document.createElement('div');
+const afterSession = document.createElement('div');
 
 // Sounds
 
@@ -35,7 +37,7 @@ let cleanHistory = true;
 let moreInfoJump = false;
 
 // Is the timer active? Paused timer is still active
-let timerActive = false;
+let timerRunning = false;
 
 // Interval timer identification for Pomodoro timer
 let timerId = null;
@@ -46,6 +48,8 @@ let moreInfoAnimation = null;
 // Stage 1 = 25 min timer, Stage 2 = 5 min timer
 let currentStage = 1;
 
+// Keep track of timer cycles to reset at 4 cycles
+let cyclesCompleted = 4;
 
 //////////////////////////////////////////////////////////
                 // Page Configuration //
@@ -96,8 +100,15 @@ stopButton.style.fontWeight = 'normal';
 stopButton.style.borderWidth = '1px';
 stopButton.addEventListener('click', () => stopTimer());
 
+// Create after timer encouragement
+//(function format for updated dynamic input)
 
-// Event handlers for instructions
+afterPomo.classList.add('text-info');
+afterPomo.innerHTML = `<p>Nice! ${cyclesCompleted} pomodoro${cyclesCompleted != 1 ? 's' : ''} down, ${4 - cyclesCompleted} to go!</p>`;
+afterSession.classList.add('text-info');
+afterSession.innerHTML = `<p>Wow, you're on fire! <br>Take a 30 minute break. You deserve it.</p>`;
+
+// Add event handlers to instructions
 instructions.addEventListener('mouseenter', () => {
     instructions.style.cursor = 'pointer';
     instructions.style.color = 'grey';
@@ -110,11 +121,12 @@ instructions.addEventListener('mouseenter', () => {
         }
     }, 500)  
 });
+
 instructions.addEventListener('mouseleave', () => {
     clearInterval(moreInfoAnimation);
     underStop.innerHTML = '';
     instructions.style.color = '#300';
-})
+});
 
 instructions.addEventListener('click', () => {
     underStop.innerHTML = '';
@@ -133,7 +145,7 @@ moreInfo.addEventListener('mouseenter', () => {
     moreInfo.style.color = 'grey';
     xBox.classList.add('x-box-shown');
     }
-});
+})
 
 moreInfo.addEventListener('mouseleave', () => {
     if (moreInfoJump) moreInfoJump = false;
@@ -141,19 +153,26 @@ moreInfo.addEventListener('mouseleave', () => {
     xBox.classList.remove('x-box-shown');
 })
 
-moreInfo.addEventListener('click' () => {
+moreInfo.addEventListener('click', () => {
     if (!moreInfoJump) {
-        buttons.classList.add('more-info-container');
-        moreInfo.classList.add('more-info-shown');
-        stop.removeChild(instructions);
-        buttons.removeChild(start25);
-        buttons.removeChild(start5);
+        buttons.classList.remove('more-info-container');
+        moreInfo.classList.remove('more-info-shown');
+        stop.appendChild(instructions);
+        buttons.appendChild(start25);
+        buttons.appendChild(start5);
     }
 })
 
 //////////////////////////////////////////////////////////
                 // Functions and Logic //
 
+/*
+Updates the encouragment message by assigning string with updated placeholders
+*/
+
+function updateEncouragement() {
+    afterPomo.innerHTML = `<p>Nice! ${cyclesCompleted} pomodoro${cyclesCompleted != 1 ? 's' : ''} down, ${4 - cyclesCompleted} to go!</p>`;
+}
 /*
 Renders a circle to signify time passed
 :param progress: <float> Number between 0 and 1 where 0 is start and 1 is finish
@@ -173,29 +192,28 @@ Starts the timer that presents progress by rerendering a circle
 :param length: <int> Length of timer in minutes
 */
 function startTimer (length) {
+    timerRunning = true;
     // Modulate color based on timer stage
     let circleColor = '#FFFFFF';
-    if (length == 5) {
+    if (length != 25) {
         circleColor = '#000000';
         currentStage = 2;
     } else if (length == 25) {
         currentStage = 1;
     }
+    
 
     // Clear timer for restart if already running
-    if (timerActive) {
+    if (!cleanHistory) {
         clearInterval(timerId);
         ball.clearRect(0, 0, ballCanvas.width, ballCanvas.height);
         length == 25 ? circleRender(1, '#000000') : circleRender(1, '#FFFFFF');
+    } else {
+        cleanHistory = false;
     }
-    timerActive = true;
-    if (timerActive) {
-        if (cleanHistory) {
-            cleanHistory = false;
-            stop.removeChild(instructions);
-        }
-        stop.appendChild(stopButton);
-    }
+
+    stopButton.innerText = 'Stop';
+    stop.replaceChild(stopButton, stop.firstElementChild);
 
     // Initialize timer variables
     const totalTime = 60000 * length;
@@ -209,33 +227,31 @@ function startTimer (length) {
         timePassed += currentRender - lastRender;
         lastRender = currentRender;
         const progress = timePassed / totalTime;
-        if (length == 5) ball.clearRect(0, 0, ballCanvas.width, ballCanvas.height);
+        if (length != 25) ball.clearRect(0, 0, ballCanvas.width, ballCanvas.height);
         circleRender(progress, circleColor);
         let newTime = new Date().getTime();
 
         // If 25 minute timer has completed, move on to 5 minute timer
         if (timePassed >= totalTime && length == 25) {
             clearInterval(timerId);
-            timerActive = false;
+            timerRunning = false;
             ball.clearRect(0, 0, ballCanvas.width, ballCanvas.height);
             currentStage = 2;
             chime25.play(); 
             startButton25.classList.remove('button-focus');
             startButton25.innerText = 'Start 25';
             startButton5.classList.add('button-focus');
-            startButton5.innerText = 'Restart 5';       
-            startTimer(5);
+            startButton5.innerText = 'Restart 5';
+            cyclesCompleted++;       
+            if (cyclesCompleted < 4) {
+                startTimer(5);
+            } else {
+                endCycle(cyclesCompleted);
+                startTimer(30);
+            }
         // If 5 minute timer has completed, revert to begining state
-        } else if (timePassed >= totalTime && length == 5) {
-            clearInterval(timerId);
-            chime5.play();
-            ball.clearRect(0, 0, ballCanvas.width, ballCanvas.height);
-            circleRender(1, '#000000', 100);
-            startButton5.classList.remove('button-focus');
-            startButton5.innerText = 'Start 5';
-            stop.removeChild(stopButton);
-            stop.appendChild(instructions);
-            cleanHistory = true;
+        } else if (timePassed >= totalTime && length != 25) {
+            endCycle(cyclesCompleted);
         }
     }, 100)
 }
@@ -246,6 +262,23 @@ Stops timer and/or chimes
 function stopTimer () {
     // Stops timer
     clearInterval(timerId);
+    if (timerRunning) {
+        stopButton.innerText = 'Reset';
+    } else {
+        ball.clearRect(0, 0, ballCanvas.width, ballCanvas.height);
+        circleRender(1, '#000000', 100);
+        if (startButton25.innerText == 'Restart 25') {
+            startButton25.innerText = 'Start 25';
+        } else {
+            startButton5.innerText = 'Start 5'
+        }
+        stop.removeChild(stopButton);
+        stop.appendChild(instructions);
+        cleanHistory = true;
+        return
+    }
+
+    timerRunning = false;
 
     // Removes button focus styling from timer buttons
     if (currentStage == 1) {
@@ -262,5 +295,23 @@ function stopTimer () {
     if (!chime5.paused) {
         chime5.pause();
         chime5.currentTime = 0;
+    }
+}
+
+function endCycle(number) {
+    clearInterval(timerId);
+    timerRunning = false;
+    chime5.play();
+    ball.clearRect(0, 0, ballCanvas.width, ballCanvas.height);
+    circleRender(1, '#000000', 100);
+    startButton5.classList.remove('button-focus');
+    startButton5.innerText = 'Start 5';
+    stop.removeChild(stopButton);
+    if (number == 4) {
+        stop.appendChild(afterSession);
+        cyclesCompleted = 0;
+    } else {
+        updateEncouragement();
+        stop.appendChild(afterPomo);
     }
 }
